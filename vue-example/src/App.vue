a15ecbfeccca82313fa5c318b92d514f4427ff32
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-11-03 13:28:02 +0100

    ADD - vite + vue

---
+++ vue-example/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Ciao mondo'
+    }
+  },
+  methods: {
+    saluta() {
+      console.log('ciao')
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="messaggio" @click="saluta">
+    {{ message }}
+  </div>
+</template>
+
+<style scoped>
+
+.messaggio {
+  color: rebeccapurple;
+}
+
+</style>
