bcfd824771f4fd5930482b3e7deae6044ed3a181
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-01-22 09:42:58 +0100

    ADD - layout ti homepage

--- vite-boolbress/src/pages/Homepage.vue
+++ vite-boolbress/src/pages/Homepage.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+  <DefaultLayout>
     <div class="container">
       <h1>{{  title }}</h1>
       <h2>{{  jobTitle }}</h2>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi molestias earum, sint repudiandae voluptates veniam sequi facilis repellendus repellat animi ipsa quibusdam necessitatibus doloribus alias harum, culpa qui expedita quod.</p>
     </div>
-  </div>
+  </DefaultLayout>
 </template>
 
 <script>
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+
 export default {
+  components: {
+    DefaultLayout
+  },
   data() {
     return {
       title: 'Gianluca',
